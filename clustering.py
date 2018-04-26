9a1850d59045d04053b3d7d1157e861332bb18f7
Patrick <sublime@vt.edu>
2018-04-26 18:40:10 -0400

    Better tweet tokenization

--- clustering.py
+++ clustering.py
@@ -2,17 +2,23 @@ import re, os, codecs, pickle
 import numpy
 import pandas
 import nltk
+from nltk.tokenize import TweetTokenizer
 # from sklearn import feature_extraction
 from sklearn.feature_extraction.text import TfidfVectorizer
 from sklearn.metrics.pairwise import cosine_similarity
 # import mpld3 # for visualization
 from EmojiFrame import EmojiFrame, getAllEmojiFrames
-
+from CSutils import millis, ask
 
 def main():
-	eFrame = getAllEmojiFrames()[0]
-	print("EmojiFrame:", eFrame.emojiName)
+	if ask("Try clustering of EmojiFrames?"):
+		eFrames = getAllEmojiFrames()
+		for ef in eFrames:
+			print("EmojiFrame:", ef.emojiName)
+			cluster(ef)
 
+
+def cluster(eFrame: EmojiFrame):
 	# dropping duplicate tweets (Retweets) removes half of tweets
 	print("Before:", eFrame.frame.shape)
 	eFrame.frame.drop_duplicates(subset='cleanTweet', inplace=True)
@@ -24,63 +30,63 @@ def main():
 	# nltk.download('punkt') # NEEDED ON FIRST RUN
 
 	cleanTweets = eFrame.frame['cleanTweet']
-	tokenCol = cleanTweets.apply(lambda row: tokenize(row))
+	tokenCol = cleanTweets.apply(lambda row: basicTokenize(row))
 	stemmedCol = tokenCol.apply(lambda row: stem(row))
-	eFrame.frame['tokenTweet'] = tokenCol
-	eFrame.frame['stemTweet'] = stemmedCol
-	# print(eFrame)
+	twTokenCol = cleanTweets.apply(lambda row: tweetTokenize(row))
+	eFrame.frame['basicTokens'] = tokenCol
+	eFrame.frame['setmmedTokens'] = stemmedCol
+	eFrame.frame['tweetTokens'] = twTokenCol
 
-	tokenVocab = set()
-	stemmedVocab = set()
-	for tokens in eFrame.frame['tokenTweet']:
-		tokenVocab.update(tokens)
-	for tokens in eFrame.frame['stemTweet']:
-		stemmedVocab.update(tokens)
+	# tokenVocab = set()
+	# stemmedVocab = set()
+	# tweetVocab = set()
+	# for tokens in eFrame.frame['basicTokens']:
+	# 	tokenVocab.update(tokens)
+	# for tokens in eFrame.frame['setmmedTokens']:
+	# 	stemmedVocab.update(tokens)
+	# for tokens in eFrame.frame['tweetTokens']:
+	# 	tweetVocab.update(tokens)
+	# #vocab_frame = pandas.DataFrame({'words': tokenVocab}, index = stemmedVocab)
+	# print('There are', str(vocab_frame.shape[0]), 'items in vocab_frame')
+	# print("Basic Token vocab:", len(tokenVocab))
+	# print("Stemmed vocab:", len(stemmedVocab))
+	# print("Tweet vocab:", len(tweetVocab))
 
-	vocab_frame = pandas.DataFrame({'words': tokenVocab}, index = stemmedVocab)
-	print('There are', str(vocab_frame.shape[0]), 'items in vocab_frame')
-
-	print("Token vocab:", len(tokenVocab))
-	print("Stemmed vocab:", len(stemmedVocab))
-
-
-	print("fitting...")
+	start = millis()
+	print("Fitting TFIDF takes... ", end='')
 	#define vectorizer parameters
-	cleanTweetsList = [str(x) for x in eFrame.frame['cleanTweet'].values.tolist()]
-	for t in cleanTweetsList:
-		a = str(t)
-
-
+	cleanTweetsList = [str(x) for x in eFrame.getCleanTweetsList()]
+	# for t in cleanTweetsList:
+	# 	a = str(t)
 
 	# stopwords = nltk.corpus.stopwords.words('english') # same as option below
-	opts = dict(max_df=0.6, max_features=200, use_idf=True)
+	opts = dict(max_features=200, use_idf=True)
 	opts.update(dict(stop_words='english', ngram_range=(1,3)))
-	opts['tokenizer'] = tokenize
+	opts['tokenizer'] = lambda x: tweetTokenize(x)
 	tfidf_vectorizer = TfidfVectorizer(**opts)
 	tfidf_matrix = tfidf_vectorizer.fit_transform(cleanTweetsList)
-	print("Done!")
+	print(millis() - start, "ms", sep='')
 
 	print("TFIDF matrix shape is", tfidf_matrix.shape)
 	terms = tfidf_vectorizer.get_feature_names()
-	print(terms)
+	print(terms[:20])
 	# dist = 1 - cosine_similarity(tfidf_matrix)
 
 
-
-def tokenizeAndStem(text):
-	return stem(tokenize(text))
+def tweetTokenize(text):
+	text = str(text)
+	tknzr = TweetTokenizer(reduce_len=True)
+	return tknzr.tokenize(text)
 
 def stem(tokens):
 	stemmer = nltk.stem.snowball.SnowballStemmer("english")
 	stems = [stemmer.stem(t) for t in tokens]
 	return stems
 
-
-def tokenize(text):
+def basicTokenize(text):
 	# first tokenize by sentence, then by word to ensure that punctuation is caught as it's own token
 	text = str(text).replace("\'", "")
 	# TODO: add to preprocessing.  apostrophes are too much to handle
-
 	tokens = [word.lower() for sent in nltk.sent_tokenize(text) for word in nltk.word_tokenize(sent)]
 
 	filtered_tokens = []

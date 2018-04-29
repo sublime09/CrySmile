77ace306321a4aa9c7178fe479bb7fb0c798027f
Patrick <sublime@vt.edu>
2018-04-29 11:13:49 -0400

    clusterVBGMM implemented; only example though.  Not hooked to tweets data

--- Clustering.py
+++ Clustering.py
@@ -114,6 +114,54 @@ def clusterMeanShift(matrix):
 	return ms
 
 
+def clusterVBGMM(matrix):
+	color_iter = cycle(['navy', 'c', 'cornflowerblue', 'gold','darkorange'])
+
+	def plot_results(X, Y_, means, covariances, index, title):
+		splot = plt.subplot(2, 1, 1 + index)
+		for i, (mean, covar, color) in enumerate(zip(means, covariances, color_iter)):
+			v, w = linalg.eigh(covar)
+			v = 2. * np.sqrt(2.) * np.sqrt(v)
+			u = w[0] / linalg.norm(w[0])
+			# as the DP will not use every component it has access to
+			# unless it needs it, we shouldn't plot the redundant components.
+			if not np.any(Y_ == i):
+				continue
+			plt.scatter(X[Y_ == i, 0], X[Y_ == i, 1], .8, color=color)
+
+			# Plot an ellipse to show the Gaussian component
+			angle = np.arctan(u[1] / u[0])
+			angle = 180. * angle / np.pi  # convert to degrees
+			ell = Ellipse(mean, v[0], v[1], 180. + angle, color=color)
+			ell.set_clip_box(splot.bbox)
+			ell.set_alpha(0.5)
+			splot.add_artist(ell)
+
+		plt.xticks(())
+		plt.yticks(())
+		plt.title(title)
+
+	# Number of samples per component
+	n_samples = 500
+
+	# Generate random sample, two components
+	np.random.seed(0)
+	C = np.array([[0., -0.1], [1.7, .4]])
+	X = np.r_[np.dot(np.random.randn(n_samples, 2), C),
+			  .7 * np.random.randn(n_samples, 2) + np.array([-6, 3])]
+
+	# Fit a Gaussian mixture with EM using five components
+	title = 'Gaussian Mixture'
+	gmm = GaussianMixture(n_components=5, covariance_type='full').fit(X)
+	plot_results(X, gmm.predict(X), gmm.means_, gmm.covariances_, 0,title)
+
+	# Fit a Dirichlet process Gaussian mixture using five components
+	title = 'Bayesian Gaussian Mixture with a Dirichlet process prior'
+	dpgmm = BayesianGaussianMixture(n_components=5,covariance_type='full').fit(X)
+	plot_results(X, dpgmm.predict(X), dpgmm.means_, dpgmm.covariances_, 1,title)
+	plt.show()
+
+
 def getTFIDFmatrix(eFrame: EmojiFrame):
 	# import nltk
 	# nltk.download('stopwords') # NEEDED ON FIRST RUN
